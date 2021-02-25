1c7523dc58349c18dd12cc36ee54c25ef87ba48f
ancoulon <ancoulon@student.s19.be>
2021-02-25 17:24:59 +0100

    updated structures

--- include/msh/parser.h
+++ include/msh/parser.h
@@ -6,7 +6,7 @@
 /*   By: ancoulon <ancoulon@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 16:03:25 by ancoulon          #+#    #+#             */
-/*   Updated: 2021/02/25 13:09:05 by ancoulon         ###   ########.fr       */
+/*   Updated: 2021/02/25 14:50:09 by ancoulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,11 @@
 
 #include "carbon/llst.h"
 
-typedef struct	s_input
+typedef struct	s_prog
 {
-	t_llst	*cmds;
-}				t_input;
-
-typedef struct	s_cmd
-{
-	t_llst	tokens;
-}				t_cmd;
+	int			argc;
+	char		**argv;
+}				t_prog;
 
 typedef struct	s_token
 {
@@ -31,15 +27,15 @@ typedef struct	s_token
 	
 }				t_token;
 
-typedef struct	s_prog
+typedef struct	s_cmd
 {
-	char		*name;
-	int			argc;
-	char		**argv;
-}				t_prog;
+	t_llst	tokens;
+}				t_cmd;
 
-
-
+typedef struct	s_input
+{
+	t_llst	*cmds;
+}				t_input;
 
 void	msh_parse(char *line);
 
