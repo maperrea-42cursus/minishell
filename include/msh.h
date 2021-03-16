c977eb9c01e55cb287e3822dc6ee6a3b8ca752b7
Antoine Coulon <antoine@30c27b.com>
2021-03-16 15:57:50 +0100

    added basis to interpreter

--- include/msh.h
+++ include/msh.h
@@ -6,16 +6,18 @@
 /*   By: ancoulon <ancoulon@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 12:59:55 by ancoulon          #+#    #+#             */
-/*   Updated: 2021/03/16 11:35:52 by ancoulon         ###   ########.fr       */
+/*   Updated: 2021/03/16 15:24:37 by ancoulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MSH_H
 # define MSH_H
 
-#include "msh/prompt.h"
-#include "msh/parser.h"
 #include "msh/shared.h"
 #include "msh/environement.h"
+#include "msh/prompt.h"
+#include "msh/parser.h"
+#include "msh/builtins.h"
+#include "msh/interpreter.h"
 
 #endif
--- source/interpreter/msh_interpreter.c
+++ source/interpreter/msh_interpreter.c
@@ -1,20 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   interpreter.h                                      :+:      :+:    :+:   */
+/*   msh_interpreter.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ancoulon <ancoulon@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/16 15:24:40 by ancoulon          #+#    #+#             */
-/*   Updated: 2021/03/16 15:26:13 by ancoulon         ###   ########.fr       */
+/*   Created: 2021/03/16 15:27:25 by ancoulon          #+#    #+#             */
+/*   Updated: 2021/03/16 15:34:10 by ancoulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef INTERPRETER_H
-# define INTERPRETER_H
+#include "msh/interpreter.h"
 
-# include "msh/parser.h"
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include "msh/shared.h"
 
-int	msh_interpreter(t_input *input);
+int	msh_interpreter(t_input *input)
+{
+	pid_t child_pid;
+	int stat_loc;
 
-#endif
+	(void)input;
+	child_pid = fork();
+	if (child_pid < 0)
+		msh_exit(1, "fork fail");
+	else if (child_pid == 0)
+	{
+		execve(NULL, NULL, NULL);
+	}
+	else
+	{
+		waitpid(child_pid, &stat_loc, WUNTRACED);
+		return (0);
+	}
+}
