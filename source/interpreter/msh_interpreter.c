ada6d73c820211a9fc9f6a1d4e8e6904c4dbbbc5
Antoine Coulon <antoine@30c27b.com>
2021-03-18 13:07:23 +0100

    interpreter executes each command

--- source/interpreter/msh_interpreter.c
+++ source/interpreter/msh_interpreter.c
@@ -6,7 +6,7 @@
 /*   By: ancoulon <ancoulon@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 15:27:25 by ancoulon          #+#    #+#             */
-/*   Updated: 2021/03/17 16:24:38 by ancoulon         ###   ########.fr       */
+/*   Updated: 2021/03/18 12:50:22 by ancoulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,26 +19,38 @@
 #include <sys/wait.h>
 #include <stdio.h>
 
-int	msh_interpreter(t_llst *cmds)
+static int execute(t_prog *prog)
 {
 	char	**envp;
 	pid_t	child_pid;
 	int		stat_loc;
 
-	(void)cmds;
 	envp = msh_env_all();
 	child_pid = fork();
 	if (child_pid < 0)
 		msh_exit(1, "fork fail");
 	else if (child_pid == 0)
 	{
-		printf("test\n");
-		char *argv[] = {"sleep", "10", NULL};
-		execve(argv[0], argv, envp);
+		execve(prog->argv[0], prog->argv, envp);
 	}
 	else
 	{
 		waitpid(child_pid, &stat_loc, WUNTRACED);
+	}
+	return (0);
+}
+
+int	msh_interpreter(t_llst *cmds)
+{
+	t_llst	*node;
+	t_prog	*prog;
+
+	node = cmds;
+	while (node)
+	{
+		prog = (t_prog *)node->data;
+		execute(prog);
+		node = node->next;
 	}
 	return (0);
 }
