448da57777186004349c4b19446fece2ddc67ac3
Antoine Coulon <antoine@30c27b.com>
2021-03-18 15:56:36 +0100

    updated msh_parse prototype

--- include/msh/parser.h
+++ include/msh/parser.h
@@ -6,7 +6,7 @@
 /*   By: ancoulon <ancoulon@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 16:03:25 by ancoulon          #+#    #+#             */
-/*   Updated: 2021/03/18 15:45:17 by ancoulon         ###   ########.fr       */
+/*   Updated: 2021/03/18 15:53:07 by ancoulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,6 @@ typedef struct	s_prog
 	int		out_fd;
 }				t_prog;
 
-int	msh_parse(char *line,t_llst	**cmds);
+int	msh_parse(char *line, t_llst **cmds);
 
 #endif
